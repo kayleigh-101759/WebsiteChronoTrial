bab75b581155bf537b159d6c0113c2fffa562647
101759 <101759@glr.nl>
2026-04-16 14:48:39 +0200

    Color

--- view/agenda_toevoegen_view.php
+++ view/agenda_toevoegen_view.php
@@ -6,12 +6,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>toevoegen</title>
+    <link rel="stylesheet" href="css/body.css">=
 
 </head>
 <body>
+<h1>Toevoegen aan Records</h1>
 <form action="./agenda_toevoegen_verwerk.php" method="POST">
     <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required >
+    <input type="text" id="username" name="username" required>
     <br><br>
 
     <label for="time">Time:</label>
@@ -22,8 +24,7 @@
     <input type="date" id="datum" name="datum" required>
     <br><br>
 
-    <br><br>
-    <input type="submit">
+    <input type="submit"><br><br>
     <a href="./agenda.php">overzicht</a>
 
 </form>
