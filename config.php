13f168c26e77c08becfd1470f86fae37b821a283
101759 <101759@glr.nl>
2026-04-16 13:41:37 +0200

    Paar files toegevoegt en de config

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+$servername ="127.0.0.1";
+$username ="Chrono101759";
+$password ="AdminChronoTrial";
+$dbname = "ChronoTrial";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+
+];
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, $options);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
